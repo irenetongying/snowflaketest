0d46f11b4e6ee4a5c62d0a452a0c35f5e9d3e473
Irene Tong <47022607+irenetongying@users.noreply.github.com>
2022-11-10 16:08:31 +0800

    Update streamlit_app.py
    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.subtitle('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Test 1')
+streamlit.text('Test 2')
+
